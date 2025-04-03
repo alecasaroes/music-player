6776685f75a24cf96f0873702d04ffaaa0f1f36b
Alexandre Casaroes <alecasaroes.dev@outlook.com>
2025-04-03 13:15:32 +0100

    add preload scripts

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron
+    // we can also expose variables, not just functions
+})
